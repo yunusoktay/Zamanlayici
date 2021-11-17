175d9f2a949c139129b27941b44e6897a8fe96ef
Yunus Oktay <ynsokty.3@gmail.com>
2021-11-17 17:13:54 +0300

    Initial Commit

---
+++ Sayaclar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sayaclar
+//
+//  Created by yunus oktay on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
