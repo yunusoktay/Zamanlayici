ed8bd6d42d9efa06ce93fe65ec5e0921a3d70d61
Yunus Oktay <ynsokty.3@gmail.com>
2021-11-17 18:03:21 +0300

    zamanlayıcı projesi yapıldı

--- Sayaclar/ViewController.swift
+++ Sayaclar/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var timer = Timer()
+    var kalanZaman = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        kalanZaman = 15
+        
+        label.text = "Zaman:\(kalanZaman)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self , selector: #selector(timerFonksiyonu) , userInfo: nil, repeats: true)
+        
+        
+        
     }
-
-
+    @objc func timerFonksiyonu() {
+        
+        label.text = "Zaman:\(kalanZaman)"
+        
+        kalanZaman = kalanZaman - 1
+        
+        if kalanZaman == 0 {
+            label.text = "Süre Bitti"
+            timer.invalidate()
+        }
+        
+    }
+    
+    @IBAction func baslatTiklandi(_ sender: Any) {
+    }
+    
 }
 
